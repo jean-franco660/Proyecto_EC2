e3c0ebb196bafe677f7a505e6e5041c79807a9c0
jean_franco <jeanfrancolaura4@gmail.com>
2025-06-24 16:39:42 -0500

    cambios en el codigo ec2

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_instance" "app_ec2" {
   instance_type               = var.instance_type
   key_name                    = var.key_name
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
-  subnet_id                   = "subnet-0b31b0b57cbc5c1cf"  # ✅ subred aws
+  subnet_id                   = "subnet-0b31b0b57cbc5c1cf" 
   associate_public_ip_address = true
   iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
 
